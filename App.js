bda6e6c3627d13052498b9842e3b06964f737095
Abner <rick.santos123@hotmail.com>
2021-12-02 18:32:48 -0300

    login

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/auth';
+import LoginAuto from './src/auth/auto';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#ff6600',
+          },
+          headerTitleAlign: 'center',
+        }}
+        >
+        <Stack.Screen
+          name="LoginAuto"
+          options={{headerShown: false}}
+          component={LoginAuto}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerBackVisible: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/auth/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import styles from './style.js';
+import url from '../utils/url.js';
+export default function Login({ navigation }){
+
+    const [telefone, setTelefone] = useState(null);
+    const [senha, setSenha] = useState(null);
+    const [mensagem, setMensagem] = useState(null);
+
+    async function acessar(){
+        if(telefone != null && senha != null){
+            if(telefone.length > 0 && senha.length > 0){
+                let response = await fetch(input=url+"loginCozinha",
+                    init = {
+                        method: "POST",
+                        headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(value={
+                            telefone: telefone,
+                            senha: senha
+                        })
+                    }
+                );
+                let json = await response.json();
+                if(json == 'erro'){
+                    setMensagem('Telefone ou senha inválido(s)!');
+                } else {
+                    setMensagem(null);
+                    try{
+                        await AsyncStorage.setItem("id", JSON.stringify(json.id));
+                        await AsyncStorage.setItem("telefone", json.telefone);
+                        await AsyncStorage.setItem("senha", json.senha);
+                        navigation.navigate("Home");
+                    } catch(e){
+                        console.log(e);
+                    }
+                }
+            } else {
+                setMensagem('Preencha o Telefone e a Senha!');    
+            }
+        } else {
+            setMensagem('Preencha o Telefone e a Senha!');
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.titulo}>Seja bem vindo!</Text>
+            <Text style={[{display: mensagem ? 'flex':'none'},styles.mensagem]}>{mensagem}</Text>
+            <Text style={styles.label}>Telefone:</Text>
+            <TextInput
+                style={styles.textInput}
+                keyboardType='phone-pad'
+                onChangeText={setTelefone}
+            />
+            <Text style={styles.label}>Senha:</Text>
+            <TextInput
+                style={styles.textInput}
+                secureTextEntry={true}
+                onChangeText={setSenha}
+            />
+            <TouchableOpacity
+                style={styles.botao}
+                onPress={()=>acessar()}>
+                <Text style={styles.textoBotao}>Acessar</Text>    
+            </TouchableOpacity>
+        </View>
+    );
+}
